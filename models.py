beb8a10853ed6cd40f31e0b1c1c40febb4a76591
jon-plazaola <jon.pccl@gmail.com>
2025-11-25 13:51:56 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    
+    name = models.CharField(max_length=50)
+    biography = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+class Book(models.Model):
+    # Un libro pertenece a una categoría solo
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    # Un libro puede tener varios autores
+    authors = models.ManyToManyField(Author)
+
+    title = models.CharField(max_length=100)
+    publication_date = models.DateField()
+
+    stock = models.IntegerField(default=0)
+
+    isbn = models.CharField(max_length=20, null=True, blank=True)
+
+    summary = models.TextField(null=True, blank=True)
+
+    #foto de portada de los libros
+    cover_image = models.ImageField(upload_to='book_covers/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
